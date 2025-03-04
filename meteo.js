d1d15a4bd31df5f56d86f4a31198150039addef7
fede1975 <federicorizzato75@gmail.com>
2025-03-04 14:20:50 +0100

    Initial Commit

---
+++ meteo.js
@@ -0,0 +1,40 @@
+let request = require('request');
+ const axios = require("axios");
+let apiKey = '71c406cd0696048af1069afda7277927';
+let city = 'Noale';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+
+
+ const express = require('express')
+const app = express()
+const port = 3000
+app.set("view engine", "ejs");
+
+
+app.get('/', async (req, res) => {
+  //res.send('prova')
+ 
+const response = await axios.get(url);
+ 
+let weather;
+
+weather = response.data;
+console.log(weather.main)
+res.render("index",{weather});
+
+
+
+})
+
+
+
+
+
+app.listen(port, () => {
+
+
+  console.log(`Example app listening on port ${port}`)
+})
+
+
